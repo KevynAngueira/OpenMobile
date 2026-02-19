aeb6b185b7fcd9fc23ccdf7c73ea3637509ecf64
ICICLE Edge Admin <icicle.edge.admin>
2026-02-18 19:32:40 -0500

    KAI: Pushing small changes

--- src/native/BuildConfigBridge.ts
+++ src/native/BuildConfigBridge.ts
@@ -2,4 +2,5 @@ import { NativeModules } from 'react-native';
 
 const { BuildConfigModule } = NativeModules;
 
-export const isDevMode = BuildConfigModule.isDevMode();
+//export const isDevMode = BuildConfigModule.isDevMode();
+export const isDevMode = true;
